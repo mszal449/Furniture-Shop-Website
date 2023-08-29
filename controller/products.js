06f452fd44ba2e424d5e0f1b223c5f6f0b257554
mszal <m.szalasz@gmail.com>
2023-08-29 21:30:42 +0200

    Added basic filtering functions to the API

--- controller/products.js
+++ controller/products.js
@@ -4,24 +4,49 @@ const { query } = require('express')
 const Product = require('../model/product')
 // static tesing route
 const getAllProductsStatic = async (req,res) => {
+  const search = 'a'
   const products = await Product.find({
-    name : "vase table" 
-  })
+    name : {$regex : search, $options : 'i'} 
+  }).sort("name")
   res.status(200).json({ products, nbHits: products.length})
 }
 
 const getAllProducts = async (req,res) => {
-
-  const {featured} = req.query
-  const queryObject = {}
-
+  const {
+    name,
+    featured,
+    company,
+    sort,
+    fields
+  } = req.query 
+  
+  const queryObject = {}  // empty query object
+  
+  if(name) {
+    queryObject.name = {$regex : name, $options: 'i'}
+  }
   if(featured) {
     queryObject.featured = featured === 'true' ? true : false
   }
+  if(company) {
+    queryObject.company = company
+  }
    
   console.log(queryObject)
-  const products = await Product.find(queryObject )
+  let result = Product.find(queryObject)
+  if (sort) {
+    const sortList = sort.split(',').join(' ')
+    result = result.sort(sortList)
+  } else {
+    result = result.sort('createdAt')
+  }
 
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ')
+    result = result.select(fieldsList)
+  }
+  
+  products = await result
   res.status(200).json({products, nbHits: products.length})
 }
 
