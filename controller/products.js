2852b907c096836c10249c948cdec8e31f98681d
mszal449 <m.szalasz@gmail.com>
2023-08-29 22:10:35 +0200

    API of website finished

--- controller/products.js
+++ controller/products.js
@@ -7,8 +7,8 @@ const getAllProductsStatic = async (req,res) => {
   const search = 'a'
   const products = await Product.find({
     name : {$regex : search, $options : 'i'} 
-  }).sort("name")
-  res.status(200).json({ products, nbHits: products.length})
+  }).sort("name").limit(7)
+  res.status(200).json({nbHits: products.length, products})
 }
 
 const getAllProducts = async (req,res) => {
@@ -17,7 +17,8 @@ const getAllProducts = async (req,res) => {
     featured,
     company,
     sort,
-    fields
+    fields,
+    numericFilters
   } = req.query 
   
   const queryObject = {}  // empty query object
@@ -32,7 +33,6 @@ const getAllProducts = async (req,res) => {
     queryObject.company = company
   }
    
-  console.log(queryObject)
   let result = Product.find(queryObject)
   if (sort) {
     const sortList = sort.split(',').join(' ')
@@ -45,7 +45,32 @@ const getAllProducts = async (req,res) => {
     const fieldsList = fields.split(',').join(' ')
     result = result.select(fieldsList)
   }
-  
+  // numeric filters
+  if (numericFilters) {
+    const operatorMap = {
+      '>' : '$gt',
+      '>=' : '$gte',
+      '=' : '$eq',
+      '<' : '$lt',
+      '<=' : '$lte',
+    }
+    const regEx = /\b(<|>|>=|<=|=)\b/g
+    let filters = numericFilters.replace(regEx, (match)=>`-${operatorMap[match]}-`)
+    const options =['price','rating']
+    filters = filters.split(',').forEach((item) => {
+      const [field, operator, value] = item.split('-')
+      if(options.included(field)) {
+        queryObject[field] = {[operator]:Number(value)}
+      }
+    });
+  }
+
+  // pagination
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit
+
+  result = result.skip(skip).limit(limit)
   products = await result
   res.status(200).json({products, nbHits: products.length})
 }
